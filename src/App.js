1ef672df68647884de9ba4076a2e9b39654da94e
Jules <dali1999@naver.com>
2023-08-28 23:40:06 +0900

    gh-pages 설치

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      {/* Route 찾음 */}
+      <Switch>
+        {/* /movie로 이동 */}
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        {/* Home으로 이동 */}
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
-function Movie({ coverImg, title, summary, genres }) {
+import { Link } from "react-router-dom";
+
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -15,7 +19,8 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
-  coverimg: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
